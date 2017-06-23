186d7573cc129e5c51a68fe08ba63f194786d7a4
Gary McLean Hall <gary.hall@fourminutemile.co.uk>
2017-06-23 12:26:22 +0100

    Moved Chapter 4 sample code to Chapter 5 & 6

---
+++ Chapter5And6/ch5-ch6-new-account-type-reward/Refactoring/ServiceTests/AccountServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Domain;
+using Domain.Factories;
+using Domain.Repositories;
+using Services;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+namespace ServiceTests
+{
+    [TestClass]
+    public class AccountServiceTests
+    {
+        [TestMethod]
+        public void CanCreateAGoldAccount()
+        {
+            // Arrange
+            var accountFactory = new Mock<IAccountFactory>();
+            var accountRepository = new Mock<IAccountRepository>();
+            var sut = new AccountService(accountFactory.Object, accountRepository.Object);
+
+            // Act
+            sut.CreateAccount(AccountType.Gold);
+
+            // Assert
+            accountRepository.Verify(ar => ar.NewAccount(It.IsAny<AccountBase>()));
+        }
+    }
+}
