9db90a50b592700bc90d535fcbf30eaf506709df
Gary McLean Hall <gary.hall@fourminutemile.co.uk>
2017-06-23 12:19:23 +0100

    Moved Chapter 7 sample code to Chapter 9

---
+++ Chapter9/ch9-lsp-subtype-contravariance/Ch7-LspVariance/SubtypeContravariance/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace SubtypeContravariance
+{
+    public class UserRepository : EntityRepository
+    {
+        // This will not compile because subtypes cannot exhibit contravariance
+        //public override void Save(object user)
+        //{
+
+        //}
+    }
+}
