a45ccb8b221e784b1c19e69bff83afa6e5d03dec
Marcel-Baltzer <35496438+Marcel-Baltzer@users.noreply.github.com>
2021-03-21 21:33:32 +0100

    add base code for example of abstraction design

---
+++ Chapter11/AbstractionDesign/AbstractionDesign/Tools/Laser.cs
@@ -0,0 +1,12 @@
+﻿namespace AbstractionDesign.Tools
+{
+    public class Laser : Sensor
+    {
+        public Laser() : base("laser") { }
+
+        public float Measure()
+        {
+            return 0f;
+        }
+    }
+}
---
+++ Chapter11/AbstractionDesign/AbstractionDesign/Tools/Sensor.cs
@@ -0,0 +1,19 @@
+﻿namespace AbstractionDesign.Tools
+{
+    public abstract class Sensor
+    {
+        protected readonly string Name;
+        protected float X, Y;
+
+        protected Sensor(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void Move(float x, float y)
+        {
+            X += x;
+            Y += y;
+        }
+    }
+}
